40bad3491a4b9c9af3ae781df73a9f8f1723e18d
simer <simer.j@gmail.com>
2018-10-18 20:01:43 +1100

    Updated ExtendedDBSpec

--- build.sbt
+++ build.sbt
@@ -9,11 +9,15 @@ resolvers += Opts.resolver.sonatypeReleases
 
 libraryDependencies ++=
   Seq(
-    "io.swaydb" %% "swaydb" % "0.5",
+    "io.swaydb" %% "swaydb" % "0.6",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2",
     "com.typesafe.akka" %% "akka-typed" % "2.5.4",
     "io.monix" %% "monix" % "3.0.0-RC1",
     "org.scalatest" %% "scalatest" % "3.0.4" % Test,
     "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0" % Test,
-    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
+    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
+    "io.circe" %% "circe-core" % "0.10.0" % Test,
+    "io.circe" %% "circe-generic" % "0.10.0" % Test,
+    "io.circe" %% "circe-parser" % "0.10.0" % Test
+
   )
--- src/test/scala/extended/ExtendedDBSpec.scala
+++ src/test/scala/extended/ExtendedDBSpec.scala
@@ -35,24 +35,25 @@ class ExtendedDBSpec extends TestBase {
     //            |____ subMap2
     //
 
-    //Create an extended database.
+    //Create an extended database by calling .extend.
+    //the Key and value should be of type Key[T] and Option[V] respectively for the extension to work.
+    //the database returns a rootMap under which all other nested maps can be created.
     val rootMap = SwayDB.memory[Key[Int], Option[String]]().assertSuccess.extend.assertSuccess
     rootMap.put(1, "root map's first key-value").assertSuccess //insert key-value to root map
 
-    val subMap1 = rootMap.putMap(1, "sub map 1").assertSuccess
+    val subMap1 = rootMap.maps.put(1, "sub map 1").assertSuccess //create the first subMap
     subMap1.put(1, "one value").assertSuccess //insert key-value to subMap
 
-    val subMap2 = subMap1.putMap(2, "sub map 2").assertSuccess //create a nested subMap under subMap1
+    val subMap2 = subMap1.maps.put(2, "sub map 2").assertSuccess //create a nested subMap under subMap1
     subMap2.put(2, "two value").assertSuccess //insert a key-value into sub-map 2
 
     subMap1.toList should contain only ((1, "one value")) //fetch all key-values of subMap1
     subMap2.toList should contain only ((2, "two value")) //fetch all key-values of subMap2
 
     //fetch all key-values of subMap1 including all the subMaps of subMap1
-    subMap1.includeMaps().toList should contain only((1, "one value"), (2, "sub map 2"))
+    subMap1.maps.toList should contain only((1, "one value"), (2, "sub map 2"))
 
     //Here only the maps of the first
-    rootMap.mapsOnly().toList should contain only ((1, "sub map 1"))
-
+    rootMap.maps.toList should contain only ((1, "sub map 1"))
   }
 }

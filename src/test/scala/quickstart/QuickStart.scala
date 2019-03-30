7e2cce6740c06076fd8a73f29c960f57674f05d1
simerplaha <simer.j@gmail.com>
2019-03-30 22:50:25 +1100

    fixed quickstart

--- src/test/scala/quickstart/QuickStart.scala
+++ src/test/scala/quickstart/QuickStart.scala
@@ -1,11 +1,16 @@
 package quickstart
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Try
+import swaydb.Wrap._
+
 object QuickStart extends App {
 
   import swaydb._
   import swaydb.serializers.Default._ //import default serializers
 
-  val db = memory.Map[Int, String]().get //Create a memory database
+  val db = memory.Map[Int, String]().get.blockingAPI[Try] //Create a memory database
 
   db.put(1, "one").get
   db.get(1).get
@@ -17,8 +22,11 @@ object QuickStart extends App {
       (key, key.toString)
   } andThen {
     keyValues =>
-      db.put(keyValues)
+      db.put(keyValues).get
   }
+
+  //write 100 key-values
+  (1 to 100) foreach { i => db.put(key = i, value = i.toString).get }
 
   //Iteration: fetch all key-values withing range 10 to 90, update values and atomically write updated key-values
   db

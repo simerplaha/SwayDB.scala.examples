8621b89e08ed35b143e553b33a3a3f81fd9da34f
simerplaha <simer.j@gmail.com>
2019-04-07 10:12:29 +1000

    updated quickstart

--- src/test/scala/quickstart/QuickStart.scala
+++ src/test/scala/quickstart/QuickStart.scala
@@ -1,26 +1,21 @@
 package quickstart
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 object QuickStart extends App {
 
   import swaydb._
   import swaydb.serializers.Default._ //import default serializers
 
-  val db = memory.Map[Int, String]().get //Create a memory database
+  val map = memory.Map[Int, String]().get //Create a memory database
 
-  db.put(1, "one").get
-  db.get(1).get
-  db.remove(1).get
+  map.put(1, "one").get
+  map.get(1).get
+  map.remove(1).get
 
   //write 100 key-values atomically
-  db.put((1 to 100).map(key => (key, key.toString)))
-
-  //write 100 key-values
-  (1 to 100) foreach { i => db.put(key = i, value = i.toString).get }
+  map.put(keyValues = (1 to 100).map(key => (key, key.toString)))
 
   //Iteration: fetch all key-values withing range 10 to 90, update values and atomically write updated key-values
-  db
+  map
     .from(10)
     .takeWhile(_._1 <= 90)
     .map {
@@ -28,6 +23,6 @@ object QuickStart extends App {
         (key, value + "_updated")
     }
     .materialize
-    .flatMap(db.put)
+    .flatMap(map.put)
     .get
 }

18e4f0ccd8a5e686ad228c4389cb49dd814f2060
simerplaha <simer.j@gmail.com>
2020-03-03 14:56:51 +1100

    ReverseOrdering

---
+++ src/test/scala/ordering/ReverseOrdering.scala
@@ -0,0 +1,28 @@
+package ordering
+
+import org.scalatest.{Matchers, WordSpec}
+import swaydb.data.order.KeyOrder
+
+class ReverseOrdering extends WordSpec with Matchers {
+
+  "reverse" in {
+    import swaydb._
+    import swaydb.serializers.Default._
+
+    implicit val reverseOrder =
+      new KeyOrder[Int] {
+        override def compare(x: Int, y: Int): Int =
+          (x compare y) * -1
+      }
+
+    val map = memory.Map[Int, Int, Nothing, Bag.Less]().get
+
+    (1 to 100).foreach(int => map.put(int, int))
+
+    map
+      .keys
+      .stream
+      .materialize[Bag.Less]
+      .toList should contain inOrderElementsOf (1 to 100).reverse
+  }
+}

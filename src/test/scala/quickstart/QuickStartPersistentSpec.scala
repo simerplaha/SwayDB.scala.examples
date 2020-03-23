6cf0f0559d002421c403f0cae3ab1a2155461645
simerplaha <simer.j@gmail.com>
2020-03-23 12:09:13 +1100

    whoops, revert!

--- src/test/scala/quickstart/QuickStartPersistentSpec.scala
+++ src/test/scala/quickstart/QuickStartPersistentSpec.scala
@@ -1,7 +1,6 @@
 package quickstart
 
 import base.TestBase
-import swaydb.data.config.{BinarySearchIndex, IOStrategy, IndexFormat}
 
 class QuickStartPersistentSpec extends TestBase {
 
@@ -13,17 +12,11 @@ class QuickStartPersistentSpec extends TestBase {
     implicit val bag = Bag.apiIO
 
     //Create a persistent database. If the directories do not exist, they will be created.
-    val db = persistent.Map[Int, String, Nothing, IO.ApiIO](
-      dir = dir.resolve("disk1"),
-      appendixFlushCheckpointSize = 2.mb,
-      binarySearchIndex = BinarySearchIndex.Disable(searchSortedIndexDirectly = true)
-    ).get
+    val db = persistent.Map[Int, String, Nothing, IO.ApiIO](dir = dir.resolve("disk1")).get
 
     db.put(1, "one").get
     db.get(1).get should contain("one")
     db.remove(1).get
-
-    //transactional/batch commit
     db.commit(
       Prepare.Put(key = 1, value = "one value"),
       Prepare.Update(from = 1, to = 100, value = "range update"),
@@ -33,7 +26,6 @@ class QuickStartPersistentSpec extends TestBase {
 
     //write 100 key-values
     (1 to 100) foreach { i => db.put(key = i, value = i.toString).get }
-
     //Iteration: fetch all key-values withing range 10 to 90, update values and batch write updated key-values
     db
       .from(10)
@@ -46,7 +38,6 @@ class QuickStartPersistentSpec extends TestBase {
       .materialize
       .flatMap(db.put)
       .get
-
     //assert the key-values were updated
     db
       .from(10)
